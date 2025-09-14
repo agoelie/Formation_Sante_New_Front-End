2fb722c0cca9a70fb4824bcbe25ab9a9111c40b1
Eliezer <kanlihanou-eliezer.ago-koucou@epitech.eu>
2025-09-14 14:56:54 +0100

    initialisation

---
+++ sante_plateforme/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
