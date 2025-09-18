cd422e8e0953390a6473d9cfa77cc6799762db25
Eliezer <kanlihanou-eliezer.ago-koucou@epitech.eu>
2025-09-18 16:17:46 +0100

    correction

--- sante_plateforme/src/components/ui/LoginForm.jsx
+++ sante_plateforme/src/components/ui/LoginForm.jsx
@@ -248,10 +248,17 @@ function App() {
           {/* Liens de connexion */}
           <div className="mt-8 text-center">
             <p className="text-gray-600">
-              Vous avez déjà un compte ?{''} 
-              <button className="text-blue-600 font-semibold hover:text-blue-700 transition-colors duration-200">
+              Vous avez déjà un compte ?{''}
+
+                        <Link to="/Register" className="flex items-center gap-2">
+                  <img src="/Formasante-logo.png" alt="Logo" className="w-20 h-10" />
+                  <button className="text-blue-600 font-semibold hover:text-blue-700 transition-colors duration-200">
+                          S'inscrire
+                        </button>
+                </Link> 
+              {/* <button className="text-blue-600 font-semibold hover:text-blue-700 transition-colors duration-200">
                 S'inscrire
-              </button>
+              </button> */}
             </p>
           </div>
         </div>
