c98f2fe1fa067a2e056b004dc711d812b4e0e6c7
Eliezer <kanlihanou-eliezer.ago-koucou@epitech.eu>
2025-09-20 13:55:35 +0100

    statistique dashboard et footer

--- sante_plateforme/src/components/ui/Footer.jsx
+++ sante_plateforme/src/components/ui/Footer.jsx
@@ -88,23 +88,35 @@ export default function Footer() {
         {/* Col 1 - Infos */}
         <div>
           <h2 className="text-2xl font-bold mb-6">FormASanté</h2>
-          <p className="text-gray-300 text-sm leading-relaxed">
+          <p className="text-white-300 text-sm leading-relaxed">
             Atteignez vos objectifs de carrière avec Santé Plus.
           </p>
           {/* Icônes sociales */}
-          <div className="flex gap-3 mt-6">
+          {/* <div className="flex gap-3 mt-6">
             {["facebook", "linkedin", "twitter", "instagram", "youtube"].map((icon) => (
               <a key={icon} href="#" className="w-10 h-10 flex items-center justify-center border border-gray-500 rounded-full hover:bg-white hover:text-cyan-900 transition">
                 <img src={`/logo${icon}.jpg`} alt={icon} className="w-5 h-5" />
               </a>
             ))}
+          </div> */}
+            <div className="flex gap-3 mt-6 bg-white">
+            {["facebook", "linkedin", "twitter", "instagram", "youtube"].map((icon) => (
+              <a
+                key={icon}
+                href="#"
+                className="w-10 h-10 flex items-center justify-center  rounded-full  hover:text-cyan-900 transition"
+              >
+                <img src={`/logo${icon}.jpg`} alt={icon} className="w-8 h-8" />
+              </a>
+            ))}
           </div>
+
         </div>
 
         {/* Col 2 - Liens rapides */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Liens rapides</h3>
-          <ul className="space-y-3 text-gray-300 text-sm">
+          <ul className="space-y-3 text-white-300 text-sm">
             <li><a href="/About">A propos</a></li>
             <li><a href="/Contact">Contact</a></li>
             <li><a href="/CoursList">Catalogue</a></li>
@@ -116,7 +128,7 @@ export default function Footer() {
         {/* Col 3 - Communauté */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Communauté</h3>
-          <ul className="space-y-3 text-gray-300 text-sm">
+          <ul className="space-y-3 text-white-300 text-sm">
             <li><a href="/Home">Ce que nous proposons</a></li>
             <li><a href="/CoursList">Formations</a></li>
              <li><a href="/Library">Bibliothèque</a></li>
@@ -129,7 +141,7 @@ export default function Footer() {
         {/* Col 4 - Newsletter */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Newsletter</h3>
-          <p className="text-gray-300 text-sm mb-4">Inscrivez-vous pour recevoir nos actualités.</p>
+          <p className="text-white-300 text-sm mb-4">Inscrivez-vous pour recevoir nos actualités.</p>
           <div className="flex bg-white rounded-lg overflow-hidden">
             <input
               type="email"
@@ -144,7 +156,7 @@ export default function Footer() {
       </div>
 
       {/* Bas de page */}
-      <div className="border-t border-gray-700 py-6 text-center text-gray-400 text-sm">
+      <div className="border-t border-gray-700 py-6 text-center text-white-400 text-sm">
         © 2025 FormASanté Inc. Tous droits réservés.
       </div>
     </footer>
--- sante_plateforme/src/components/ui/LibraryFooter.jsx
+++ sante_plateforme/src/components/ui/LibraryFooter.jsx
@@ -159,32 +159,115 @@
 // }
 
 
+// export default function Footer() {
+//   return (
+//     <footer className="bg-blue-800 text-white">
+//       {/* Section principale */}
+//       <div className="max-w-7xl mx-auto px-6 py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
+        
+//         {/* Col 1 - Infos */}
+//         <div>
+//           <h2 className="text-2xl font-bold mb-6">FormASanté</h2>
+//           <p className="text-gray-300 text-sm leading-relaxed">
+//             Atteignez vos objectifs de carrière avec Santé Plus.
+//           </p>
+//           {/* Icônes sociales */}
+//           <div className="flex gap-3 mt-6">
+//             {["facebook", "linkedin", "twitter", "instagram", "youtube"].map((icon) => (
+//               <a key={icon} href="#" className="w-10 h-10 flex items-center justify-center border border-gray-500 rounded-full hover:bg-white hover:text-cyan-900 transition">
+//                 <img src={`/logo${icon}.jpg`} alt={icon} className="w-5 h-5" />
+//               </a>
+//             ))}
+//           </div>
+//         </div>
+
+//         {/* Col 2 - Liens rapides */}
+//         <div>
+//           <h3 className="text-xl font-semibold mb-6">Liens rapides</h3>
+//           <ul className="space-y-3 text-gray-300 text-sm">
+//             <li><a href="/About">A propos</a></li>
+//             <li><a href="/Contact">Contact</a></li>
+//             <li><a href="/CoursList">Catalogue</a></li>
+//             <li><a href="/About">Direction</a></li>
+//             <li><a href="/About">Pour les entreprises</a></li>
+//           </ul>
+//         </div>
+
+//         {/* Col 3 - Communauté */}
+//         <div>
+//           <h3 className="text-xl font-semibold mb-6">Communauté</h3>
+//           <ul className="space-y-3 text-gray-300 text-sm">
+//             <li><a href="/CoursList">Ce que nous proposons</a></li>
+//             <li><a href="/CoursList">Formations</a></li>
+//             <li><a href="/About">Partenaires</a></li>
+//             <li><a href="/Contact">Conditions</a></li>
+//           </ul>
+//         </div>
+
+//         {/* Col 4 - Newsletter */}
+//         <div>
+//           <h3 className="text-xl font-semibold mb-6">Newsletter</h3>
+//           <p className="text-gray-300 text-sm mb-4">Inscrivez-vous pour recevoir nos actualités.</p>
+//           <div className="flex bg-white rounded-lg overflow-hidden">
+//             <input
+//               type="email"
+//               placeholder="Votre email"
+//               className="px-4 py-2 text-gray-800 flex-1 focus:outline-none"
+//             />
+//             <button className="bg-red-500 px-5 text-white font-semibold hover:bg-orange-600 transition">
+//               S'inscrire
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* Bas de page */}
+//       <div className="border-t border-gray-700 py-6 text-center text-white-400 text-sm">
+//         © 2025 FormASanté Inc. Tous droits réservés.
+//       </div>
+//     </footer>
+//   );
+// }
+
+
 export default function Footer() {
   return (
-    <footer className="bg-blue-800 text-white">
+    <footer className="bg-blue-700 text-white">
       {/* Section principale */}
       <div className="max-w-7xl mx-auto px-6 py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
         
         {/* Col 1 - Infos */}
         <div>
           <h2 className="text-2xl font-bold mb-6">FormASanté</h2>
-          <p className="text-gray-300 text-sm leading-relaxed">
+          <p className="text-white-300 text-sm leading-relaxed">
             Atteignez vos objectifs de carrière avec Santé Plus.
           </p>
           {/* Icônes sociales */}
-          <div className="flex gap-3 mt-6">
+          {/* <div className="flex gap-3 mt-6">
             {["facebook", "linkedin", "twitter", "instagram", "youtube"].map((icon) => (
               <a key={icon} href="#" className="w-10 h-10 flex items-center justify-center border border-gray-500 rounded-full hover:bg-white hover:text-cyan-900 transition">
                 <img src={`/logo${icon}.jpg`} alt={icon} className="w-5 h-5" />
               </a>
             ))}
+          </div> */}
+            <div className="flex gap-3 mt-6 bg-white">
+            {["facebook", "linkedin", "twitter", "instagram", "youtube"].map((icon) => (
+              <a
+                key={icon}
+                href="#"
+                className="w-10 h-10 flex items-center justify-center  rounded-full  hover:text-cyan-900 transition"
+              >
+                <img src={`/logo${icon}.jpg`} alt={icon} className="w-8 h-8" />
+              </a>
+            ))}
           </div>
+
         </div>
 
         {/* Col 2 - Liens rapides */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Liens rapides</h3>
-          <ul className="space-y-3 text-gray-300 text-sm">
+          <ul className="space-y-3 text-white-300 text-sm">
             <li><a href="/About">A propos</a></li>
             <li><a href="/Contact">Contact</a></li>
             <li><a href="/CoursList">Catalogue</a></li>
@@ -196,9 +279,11 @@ export default function Footer() {
         {/* Col 3 - Communauté */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Communauté</h3>
-          <ul className="space-y-3 text-gray-300 text-sm">
-            <li><a href="/CoursList">Ce que nous proposons</a></li>
+          <ul className="space-y-3 text-white-300 text-sm">
+            <li><a href="/Home">Ce que nous proposons</a></li>
             <li><a href="/CoursList">Formations</a></li>
+             <li><a href="/Library">Bibliothèque</a></li>
+
             <li><a href="/About">Partenaires</a></li>
             <li><a href="/Contact">Conditions</a></li>
           </ul>
@@ -207,7 +292,7 @@ export default function Footer() {
         {/* Col 4 - Newsletter */}
         <div>
           <h3 className="text-xl font-semibold mb-6">Newsletter</h3>
-          <p className="text-gray-300 text-sm mb-4">Inscrivez-vous pour recevoir nos actualités.</p>
+          <p className="text-white-300 text-sm mb-4">Inscrivez-vous pour recevoir nos actualités.</p>
           <div className="flex bg-white rounded-lg overflow-hidden">
             <input
               type="email"
@@ -222,7 +307,7 @@ export default function Footer() {
       </div>
 
       {/* Bas de page */}
-      <div className="border-t border-gray-700 py-6 text-center text-gray-400 text-sm">
+      <div className="border-t border-gray-700 py-6 text-center text-white-400 text-sm">
         © 2025 FormASanté Inc. Tous droits réservés.
       </div>
     </footer>
